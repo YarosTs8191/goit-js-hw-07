9b2c3f6eef74eaae1b8cb015be25575e088206a8
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2025-02-15 12:45:16 +0100

    task-4-5

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,22 @@
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Запобігаємо перезавантаженню сторінки
 
+    const form = event.currentTarget;
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
+
+    if (!email || !password) {
+      alert("All form fields must be filled in");
+      return;
+    }
+
+    const formData = {
+      email,
+      password,
+    };
+
+    console.log(formData);
+    form.reset(); // Очищуємо форму після успішного сабміту
+  });
